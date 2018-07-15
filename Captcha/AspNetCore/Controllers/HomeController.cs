d8e64b6e8819b6157a1df0d40aa7885445748741
H.Hajisharifi <hassan@hajisharifi.com>
2018-07-15 21:37:29 +0430

    initialize

---
+++ Captcha/AspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Html;
+
+using Diamond.AspNetCore.Html.Captcha;
+
+namespace H2.ASP_Captcha.Controllers
+{
+    public class HomeController
+        : Controller
+    {
+        //________________________________________________________________________
+
+        private readonly ICachedCaptcha m_Captcha;
+        //________________________________________________________________________
+
+        public HomeController(ICachedCaptcha captcha)
+        {
+            this.m_Captcha = captcha;
+        }
+        //________________________________________________________________________
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+        //________________________________________________________________________
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string Captcha_RefID, string Input_Captcha)
+        {
+            bool isValid = this.m_Captcha.Validate(Captcha_RefID, Input_Captcha);
+
+            if (isValid)
+            {
+                this.ViewBag.Message = new HtmlString(@"<span class=""captcha-ok"">OK</span>");
+            }
+            else
+            {
+                this.ViewBag.Message = new HtmlString(@"<span class=""captcha-error"">ERROR</span>");
+            }
+
+            return this.View();
+        }
+        //________________________________________________________________________
+
+        public IActionResult CaptchaImage(string id, int version = 0)
+        {
+            return this.m_Captcha.GetImage(id, version);
+        }
+        //________________________________________________________________________
+    }
+}
---
+++ Captcha/AspNetCore/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace H2.ASP_Captcha
+{
+    public static class Program
+    {
+        //________________________________________________________________________
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+        //________________________________________________________________________
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost
+                .CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+        }
+        //________________________________________________________________________
+    }
+}
